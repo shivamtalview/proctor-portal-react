435cfaea6d542942fc038b972805e5c2a8a718c5
Harish Gnanasekar <harish.gnanasekar@talview.com>
2026-08-04 15:51:35 +0530

    Remove llm implementation files

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/proctor-portal-react/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
